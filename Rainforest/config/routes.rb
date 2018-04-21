5db8a96c8fdd548f355abc6c0bddf1433169f5d5
Hasan Naseem <hn92@live.com>
2018-04-21 18:50:21 -0400

    defined and improved route syntax

--- Rainforest/config/routes.rb
+++ Rainforest/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  resource :users, only: %i(new create)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
   resources :products do
-    resources :reviews, only: [:create, :edit, :destroy, :new, :update]
+    resources :reviews, only: %i(create edit destroy new update)
   end
 
 
