91211499bda85b10218db7cc754ba84e6c4a20cf
Hasan Naseem <hn92@live.com>
2018-04-21 18:43:42 -0400

    ran migration for User table

--- Rainforest/db/schema.rb
+++ Rainforest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_185453) do
+ActiveRecord::Schema.define(version: 2018_04_21_224201) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 2018_04_19_185453) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "product_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.integer "balance"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
